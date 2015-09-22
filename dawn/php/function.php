cd35d0a92b9d29b81ba060770b8d450beb049368
Andrew Griesemer <ajgriesemer@gmail.com>
2015-09-21 19:36:51 -0500

    test

--- dawn/php/function.php
+++ dawn/php/function.php
@@ -1,3 +1,15 @@
 <?php
-echo https://api.spark.io/v1/devices/54ff6d066672524818311167/controlPanel
+$method = $_REQUEST["method"];
+if(!$method) die("device or method param missing\n");
+
+// add access token
+$_REQUEST["access_token"] = "12399....123123";
+
+// create params
+$params = http_build_query($_REQUEST);
+
+// make request
+$url = "https://api.spark.io/v1/devices/54ff6d066672524818311167/" . $method . "?" . $params;
+$response = file_get_contents($url);
+echo $response; 
 ?>
